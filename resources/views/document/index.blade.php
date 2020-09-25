1d0d2433133472876a553249980d58b0fc4964e0
miyamaruyuuki <ajptmjg@gmail.com>
2020-09-25 14:30:00 +0900

    1429

--- resources/views/document/index.blade.php
+++ resources/views/document/index.blade.php
@@ -6,20 +6,5 @@
 <body>
 <h1>技術文書管理</h1>
 
-<form action="/" method="get">
-<div>検索ワード：<input type="text" name="searchWord" value="@if(isset($searchWord)) {{$searchWord}} @endif"><input type="submit" value="検索する"></div>
-</form>
-@if(isset($cid))
-<a href="{{ url('/') }}">絞り込み初期化</a>
-@endif
-<div><a href="{{url('documentAdd')}}">文書追加</a></div>
-<table>
-    @foreach ($documentList as $data)
-        <tr><td>{{$data->registdate}}</td><td><a href="{{url('list/' .$data->cid)}}">{{$data->cname}}</a></td><td><a href="{{url('update/' .$data->did)}}">{{$data->title}}</a></td></tr>
-    @endforeach
-</table>
-@if($documentList->isEmpty())
-    <p>見つかりません</p>
-@endif
-</body>
+
 </html>
